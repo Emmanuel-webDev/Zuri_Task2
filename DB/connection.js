5f05051065eaa572b7df5ebb206bc545eb3ae4cd
Emmanuel Onuorah <emmanuelonuorah875@gmail.com>
2023-09-13 11:38:53 +0100

    Fixed issue causing deployment to fail

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connectDb(){
-    mongoose.connect("mongodb://127.0.0.1:27017/express-mongoose", { UseNewUrlParser: true })
+    mongoose.connect(process.env.URL, { UseNewUrlParser: true })
     console.log("Connected successfully")
 }
 
